3e4942087f8aa7f7cf4a21d3ff24661f6ade607c
Raul Ismagilov <smaggy1337@gmail.com>
2025-03-11 18:29:02 +0300

    start adding unsplash API

---
+++ Image Feed/Constans.swift
@@ -0,0 +1,15 @@
+//
+//  Constans.swift
+//  Image Feed
+//
+//  Created by Рауль on 11.03.2025.
+//
+import UIKit
+
+enum Constans {
+    static let accessKey = "cvNi-HxZRTqF7KwhVTra-6sOpci2hXkOi9HQlsHUOmA"
+    static let secretKey = "MMgeAZc7gHZrQUtFarAVHGgAc9tRr8K1PlPql2IfFsw"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
--- Image Feed/Profile/ProfileViewController.swift
+++ Image Feed/Profile/ProfileViewController.swift
@@ -1,13 +1,91 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    @IBOutlet private var avatarImageView: UIImageView!
-    @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var loginNameLabel: UILabel!
-    @IBOutlet private var descriptionLabel: UILabel!
-
-    @IBOutlet private var logoutButton: UIButton!
-
-    @IBAction private func didTapLogoutButton() {
+    private var avatarImageView: UIImageView!
+    private var nameLabel: UILabel!
+    private var loginNameLabel: UILabel!
+    private var descriptionLabel: UILabel!
+    
+    private var logoutButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let profileImage = UIImage(named: "avatar")
+        let avatarImageView = UIImageView(image: profileImage)
+        avatarImageView.contentMode = .scaleAspectFit
+        self.avatarImageView = avatarImageView
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(avatarImageView)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 70),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 70)
+        ])
+        
+        
+        let nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.font = .systemFont(ofSize: 23, weight: .semibold)
+        nameLabel.textColor = .white
+        view.addSubview(nameLabel)
+        self.nameLabel = nameLabel
+        
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
+            nameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 8),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 16)
+        ])
+        
+        
+        let loginNameLabel = UILabel()
+        loginNameLabel.text = "@ekaterina_nov"
+        loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        loginNameLabel.font = .systemFont(ofSize: 13)
+        loginNameLabel.textColor = .gray
+        view.addSubview(loginNameLabel)
+        self.loginNameLabel = loginNameLabel
+        
+        NSLayoutConstraint.activate([
+            loginNameLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            loginNameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8)
+        ])
+        
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.font = .systemFont(ofSize: 13)
+        descriptionLabel.textColor = .white
+        view.addSubview(descriptionLabel)
+        self.descriptionLabel = descriptionLabel
+        
+        NSLayoutConstraint.activate([
+            descriptionLabel.trailingAnchor.constraint(equalTo: loginNameLabel.trailingAnchor),
+            descriptionLabel.leadingAnchor.constraint(equalTo: loginNameLabel.leadingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8)
+        ])
+        
+        
+        let logoutButton = UIButton(type: .custom) // Используем кастомную кнопку
+        logoutButton.setImage(UIImage(named: "logout_button"), for: .normal)
+        logoutButton.addTarget(self, action: #selector(didTapLogoutButton), for: .touchUpInside)
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoutButton)
+        self.logoutButton = logoutButton
+        
+        NSLayoutConstraint.activate([
+            logoutButton.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: logoutButton.trailingAnchor, constant: 16)
+        ])
+    }
+    
+    @objc private func didTapLogoutButton() {
+        print("Кнопка 'Выйти' нажата!")
     }
 }
